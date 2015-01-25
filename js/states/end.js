64119099abcb3450485a50beaf2857e054e4700b
Jakob Willforss <jakob.willforss@hotmail.com>
2015-01-25 13:05:21 +0100

    Updates ending scene

--- js/states/end.js
+++ js/states/end.js
@@ -14,18 +14,21 @@ end.prototype = {
         var style = { font: 'bold 45pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
         var style2 = { font: 'bold 24pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
 
-        var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, "You Are Clogged!", style);
-        text.anchor.set(0.5);
+        var text1 = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, "You Are Clogged!", style);
+        text1.anchor.set(0.5);
         
-        var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 100, "Press [space] to restart", style2);
-        text.anchor.set(0.5);
+        var string = "..after " + this.game.latest_time + " seconds";
+        var text2 = this.game.add.text(this.game.world.centerX, this.game.world.centerY, string, style2);
+        text2.anchor.set(0.5);
+        
+        var text3 = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 100, "Press [F5] to restart", style2);
+        text3.anchor.set(0.5);
 
     },
     update: function() {
-        var key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        if (key.isDown) {
-            this.game.state.start('Menu');
-        }
+//        var key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+//        if (key.isDown) {
+//            this.game.state.start('Menu');
+//        }
     }
-    
 };
